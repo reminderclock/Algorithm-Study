8b01e7cf4b673297b954a0d53fd255bc23d42c80
reminderclock <reminderclock@naver.com>
2020-12-02 23:37:11 +0900

    20/12/02

--- Programmers/Level1.js
+++ Programmers/Level1.js
@@ -727,3 +727,16 @@ function sumMatrix(A,B){
     console.log(solution([[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]],[1,5,3,5,1,2,1,4]));
 }
 
+//완주하지 못한 선수 
+{
+    function solution(participant, completion) {
+        participant.sort();
+        completion.sort();
+
+        for(let i in participant) {
+            if(participant[i] !== completion[i])
+                return participant[i];
+        }
+    }
+    console.log(solution(['leo', 'kiki', 'eden'], ['eden', 'kiki']));
+}
