dd16f9d4991aeaaa9935107369af849068e9b8d5
reminderclock <reminderclock@naver.com>
2021-01-10 23:50:39 +0900

    Fix: [문1009] 분산처리_시간초과

--- Baekjoon/1009/1009.js
+++ Baekjoon/1009/1009.js
@@ -1,23 +1,31 @@
-const fs = require("fs");
-// const input = fs.readFileSync("dev/stdin").toString().trim().split("/n");
-const input = fs
-.readFileSync("./stdin.txt")
-.toString()
-.trim()
-.split("\r\n");
-const a = input.map((num) =>num.split(' '));
-a.shift();
-const arr=[];
-a.forEach( (num) =>{
-    if((num[0][num[0].length-1])==='0'&& (num[1][num[1].length-1])==='0'){
-        return arr.push(0);
-    }
-    arr.push(Math.pow((num[0][num[0].length-1]),(num[1][num[1].length-1])))
-});
-const lastInfo=arr.map( (num) => num.toString()[num.toString().length-1]);
-lastInfo.forEach((num)=>{
-    if(num == 0){
-        num = 10;
-    }
-    console.log(parseInt(num));
-});
+// const fs = require("fs");
+// // const input = fs.readFileSync("dev/stdin").toString().trim().split("/n");
+// const input = fs
+// .readFileSync("./stdin.txt")
+// .toString()
+// .trim()
+// .split("\r\n");
+function solution(numArr) {
+    numArr.forEach(el => {
+        let line = 1;
+        for(let i=0; i<parseInt(el[1]); i++) {
+            line = parseInt((line*el[0]).toString()[(line*el[0]).toString().length-1]);
+        }
+        if(line === 0) {
+            line = 10;
+        }
+        console.log(line)
+    });
+}
+const data = `5
+1 6
+3 7
+6 2
+7 100
+9 635
+10 2
+10 10`;
+const input = data.toString().split("\n");
+const cnt =parseInt(input.shift());
+const numArr = input.map((num) => num.split(' '));
+solution(numArr);
