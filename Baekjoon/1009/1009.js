aaa000e23a9d151b9a2ff8b4b3a6deb6ad90de4d
reminderclock <reminderclock@naver.com>
2021-01-11 01:32:42 +0900

    Fix:[1009] 분산처리 시간초과 해결

--- Baekjoon/1009/1009.js
+++ Baekjoon/1009/1009.js
@@ -5,18 +5,6 @@
 // .toString()
 // .trim()
 // .split("\r\n");
-function solution(numArr) {
-    numArr.forEach(el => {
-        let line = 1;
-        for(let i=0; i<parseInt(el[1]); i++) {
-            line = parseInt((line*el[0]).toString()[(line*el[0]).toString().length-1]);
-        }
-        if(line === 0) {
-            line = 10;
-        }
-        console.log(line)
-    });
-}
 const data = `5
 1 6
 3 7
@@ -28,4 +16,12 @@ const data = `5
 const input = data.toString().split("\n");
 const cnt =parseInt(input.shift());
 const numArr = input.map((num) => num.split(' '));
-solution(numArr);
+numArr.forEach((v) => solution(v));
+
+function solution(line) {
+  let [a, b] = line.map((v) => parseInt(v));
+  a = a % 10;
+  b = b % 4 ? b % 4 : 4;
+  let answer = Math.pow(a, b) % 10 ? Math.pow(a, b) % 10 : 10;
+  console.log(answer);
+}
