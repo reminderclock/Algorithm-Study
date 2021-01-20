0aa37d69d1e9ddbdd65baaf0fd86f27df10463b8
reminderclock <reminderclock@naver.com>
2021-01-20 17:20:48 +0900

    Fix: [LeetCode]common-prefix.js 오류해결

--- LeetCode/common-prefix.js
+++ LeetCode/common-prefix.js
@@ -1,32 +1,20 @@
 var longestCommonPrefix = function(strs) {
-    // let a= strs[0].lengt;
-    if(strs.length === 0) {
-        return "";
-    }
-    for(let k=0; k<strs.length; k++) {
-        if(strs[k].length ===0) {
-            return "";
-        }
-    }
-    if(strs.length === 1) {
+    if([...new Set(strs)].length ===1){
         return strs[0];
     }
-    if([...new Set(strs)].length ===1){
-        return str[0];
-    }
-    for(let j=0; j<100; j++) {
+    for(let j=0; j<=200; j++) {
         for(let i=1; i<strs.length; i++) {
             if(strs[0][j] !== strs[i][j]) {
-                if(j === 0) {
-                    return "";
-                }
-                return strs[0].slice(0,i);
+                return strs[0].slice(0,j);
             }
         }
     }
+    return "";
 };
 console.log(longestCommonPrefix(["flower","flow","flight"]));
 console.log(longestCommonPrefix(["dog","racecar","car"]));
 console.log(longestCommonPrefix([""]));
 console.log(longestCommonPrefix([])); 
 console.log(longestCommonPrefix(["a"])); 
+console.log(longestCommonPrefix(["a","a","a"])); 
+console.log(longestCommonPrefix(["aaa" ,"aa", "aaa"])); 
