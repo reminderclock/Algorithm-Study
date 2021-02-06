fa5666bd28bfd049ab354f80b0ab546495fe885d
reminderclock <reminderclock@naver.com>
2021-02-06 23:53:44 +0900

    Fix: 66. q블록쌓기

--- Code_festival/66.js
+++ Code_festival/66.js
@@ -1,14 +1,27 @@
 // 블럭탑 쌓기
-function solution(arr, rule) {
-    let result = [];
-    for(let i=0; i<arr.length; i++) {
-        for(let j=0; j<arr[i].length; j++) {
-        if(!rule.includes(arr[i][j])) {
-            arr[i][j].substr(1);
+function solution(전체블록, 규칙){
+    let answer = [];
+    for (let 부분블록 of 전체블록){
+      answer.push(블록순서체크(부분블록, 규칙));
+    }
+    return answer;
+  }
+  
+  function 블록순서체크(부분블록, 규칙){
+    let 임시변수 = 규칙.indexOf(규칙[0]);
+    for (let 문자 of 부분블록){
+      if (규칙.includes(문자)){
+        if (임시변수 > 규칙.indexOf(문자)){
+          return '불가능';
         }
+        임시변수 = 규칙.indexOf(문자);
+      }
     }
-    }
-    // for(let j=0; j<arr.l)
-    return arr;
-}
-console.log(solution(["ABCDEF", "BCAD", "ADEFQRX", "BEDFG", "EFGHZ"], "ABD"));
+    return '가능';
+  }
+  
+  
+  const 전체블록 = ['ABCDEF', 'BCAD', 'ADEFQRX', 'BEDFG', 'AEBFDGCH'];
+  const 규칙 = 'ABCD';
+  
+  console.log(solution(전체블록, 규칙));
