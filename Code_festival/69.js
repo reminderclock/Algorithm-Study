340c7f8b80bd38ad31035c47adc1a8837da7d750
reminderclock <reminderclock@naver.com>
2021-02-09 23:58:52 +0900

    add: 69.js 골드바흐

---
+++ Code_festival/69.js
@@ -0,0 +1,10 @@
+//골드바흐의 추축
+function solution(s) {
+    let arr = [1, parseInt(s)-1];
+    for(let i=1; i<50; i++) {
+        arr = [arr[0]+i,arr[1]-i];
+        if()
+    }
+
+}
+console.log(solution('100'))
