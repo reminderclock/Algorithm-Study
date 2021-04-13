00e8b553a541c48942184b42eb6aba5844c62d6c
reminderclock <reminderclock@naver.com>
2021-04-13 22:56:27 +0900

    feat: 소수찾기

---
+++ Programmers/Level1/sosu.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    const suso= new Array(n).fill(true);
+    suso[0] = false;
+    for (let i = 2; i ** 2 <= n; i++) {
+      if (suso[i - 1] === true) {
+        for (let j = i ** 2; j <= n; j += i) {
+          suso[j - 1] = false;
+        }
+      }
+    }
+    return suso.filter((e) => e).length;
+  }
