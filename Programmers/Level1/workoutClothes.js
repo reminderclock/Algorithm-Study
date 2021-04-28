d04735a6f4ec9a9a63f6a8b87c79b0cf4cbb5f46
reminderclock <reminderclock@naver.com>
2021-04-28 22:46:35 +0900

    feat: level1 체육복(탐욕법

---
+++ Programmers/Level1/workoutClothes.js
@@ -0,0 +1,17 @@
+function solution(n, lost, reserve) {
+    let realReserve = reserve.filter(r => !lost.includes(r));
+    let realLost = lost.filter(r => !reserve.includes(r));
+
+    for(let i=0; i<realReserve.length; i++) {
+        for(let j=0; j<realLost.length; j++) {
+            if(Math.abs(realReserve[i]-realLost[j])===1 && realReserve[i]!==0 && realLost[j]!==0) {
+                realLost.splice(j,1);
+                realReserve.splice(i,1);
+                j=0;
+                i=0;
+            }
+        }
+    }
+    return n-realLost.length;
+}
+console.log(solution(5, [2,4], [1,3,5]))
