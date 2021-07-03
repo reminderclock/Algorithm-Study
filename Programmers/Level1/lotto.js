1d5ea0a1061add358e51e4cb31100dd0f586164c
reminderclock <reminderclock@naver.com>
2021-07-03 19:05:34 +0900

    feat: prog level1 lotto 2

--- Programmers/Level1/lotto.js
+++ Programmers/Level1/lotto.js
@@ -1,8 +1,18 @@
+// function solution(lottos, win_nums) {
+//     let rank = [6,6,5,4,3,2,1];
+//     const min = lottos.filter(e => win_nums.includes(e)).length
+//     const max = lottos.filter(e => e === 0).length + min;
+//     return [rank[max], rank[min]]
+// }
 function solution(lottos, win_nums) {
-    let rank = [6,6,5,4,3,2,1];
-    const min = lottos.filter(e => win_nums.includes(e)).length
-    const max = lottos.filter(e => e === 0).length + min;
-    return [rank[max], rank[min]]
+    const rank = [6,6,5,4,3,2,1];
+    const min = [];
+    const zero = [];
+    lottos.map(e => {
+        if(win_nums.includes(e)) min.push(0);
+        if(e === 0) zero.push(0);
+    })
+    return [rank[zero.length+min.length], rank[min.length]]
 }
 
 console.log(solution([44, 1, 0, 0, 31, 25], [31, 10, 45, 1, 6, 19]))
