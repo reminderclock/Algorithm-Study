faf15addca37af5053941b64a29a3279c09977b2
reminderclock <reminderclock@naver.com>
2021-07-03 16:15:08 +0900

    feat: prog level1 lotto refactor

--- Programmers/Level1/lotto.js
+++ Programmers/Level1/lotto.js
@@ -1,23 +1,30 @@
-function solution(lottos, win_nums) {
-    let min= 0;
-    let max = 0;
-    let arr= [6,5,4,3,2,1];
-    for(let i=0; i<lottos.length; i++) {
-        if(win_nums.includes(lottos[i])) {
-            min++;
-        }
-        if(lottos[i]===0) {
-            max++;
-        }
-    }
-    max+=min;
-    if(min<=1)min=6;
-    else min = arr.indexOf(min)+1;
-    if(max<=1)max=6;
-    else max = arr.indexOf(max)+1;
-    let arr2= [max, min];
-    return arr2;
+// function solution(lottos, win_nums) {
+//     let min= 0;
+//     let max = 0;
+//     let arr= [6,5,4,3,2,1];
+//     for(let i=0; i<lottos.length; i++) {
+//         if(win_nums.includes(lottos[i])) {
+//             min++;
+//         }
+//         if(lottos[i]===0) {
+//             max++;
+//         }
+//     }
+//     max+=min;
+//     if(min<=1)min=6;
+//     else min = arr.indexOf(min)+1;
+//     if(max<=1)max=6;
+//     else max = arr.indexOf(max)+1;
+//     let arr2= [max, min];
+//     return arr2;
 
+// }
+function solution(lottos, win_nums) {
+    let rank = [6,6,5,4,3,2,1];
+    const min = lottos.filter(e => win_nums.includes(e)).length
+    const max = lottos.filter(e => e === 0).length + min;
+    return [rank[max], rank[min]]
 }
+
 console.log(solution([44, 1, 0, 0, 31, 25], [31, 10, 45, 1, 6, 19]))
-console.log(solution([7,8,9,10,11,12], [1,2,3,4,5,6]))
+console.log(solution([7,8,9,10,11,12], [1,2,3,4,5,6]));
