bcf643936ae294e7760e96d6c69163b32b5c1b20
reminderclock <reminderclock@naver.com>
2021-07-04 23:15:54 +0900

    feat: [Programmers] 폰켓몬2

--- Programmers/Level1/phonecatmon.js
+++ Programmers/Level1/phonecatmon.js
@@ -1,12 +1,19 @@
+// function solution(nums) {
+//     var answer = 0;
+//     let set2 = new Set(nums)
+//     if([...set2].length > nums.length/2) {
+//         answer = nums.length/2;
+//     }
+//     else answer = [...set2].length;
+//     return answer;
+// }
+
 function solution(nums) {
-    var answer = 0;
-    let set2 = new Set(nums)
-    if([...set2].length > nums.length/2) {
-        answer = nums.length/2;
-    }
-    else answer = [...set2].length;
-    return answer;
+    const max = nums.length/2;
+    const uniqueCount = new Set(nums).size
+    return (uniqueCount > max) ? max: uniqueCount;
 }
+
 console.log(solution([3,1,2,3]))
 console.log(solution([3,3,3,2,2,4]))
 console.log(solution([3,3,3,2,2,2]))
