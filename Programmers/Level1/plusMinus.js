710f7ef17f9028ec5e7eab048b85c0b1cf307b7e
reminderclock <reminderclock@naver.com>
2021-07-05 15:51:44 +0900

    docs: 음양 로직

--- Programmers/Level1/plusMinus.js
+++ Programmers/Level1/plusMinus.js
@@ -6,6 +6,9 @@
 //     })
 //     return sum;
 // }
+// 로직
+// 1. 해당 배열을 반복문을 통해 더한다.
+// 2. 더해줄때 부호를 셋팅해준다.
 function solution(absolutes, signs) {
     return absolutes.reduce((acc, value, index) => acc + (value*(signs[index]?1:-1)), 0)
 }
