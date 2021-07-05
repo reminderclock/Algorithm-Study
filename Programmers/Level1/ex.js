54e97a090af80783ff7b13fd1884253f7cb4bab3
reminderclock <reminderclock@naver.com>
2021-07-05 15:48:57 +0900

    feat: [programmers] 음양 더하기2

--- Programmers/Level1/ex.js
+++ Programmers/Level1/ex.js
@@ -1,9 +1,4 @@
 function solution(absolutes, signs) {
-    let sum = 0;
-    absolutes.map((e,index) => {
-        if(signs[index]) sum += e;
-        else sum += -e;
-    })
-    return sum;
+    return absolutes.reduce((acc, value, index) => acc + (value*(signs[index]?1:-1)), 0)
 }
 console.log(solution([4,7,12], [true,false,true]))
--- Programmers/Level1/plusMinus.js
+++ Programmers/Level1/plusMinus.js
@@ -1,9 +1,12 @@
+// function solution(absolutes, signs) {
+//     let sum = 0;
+//     absolutes.map((e,index) => {
+//         if(signs[index]) sum += e;
+//         else sum += -e;
+//     })
+//     return sum;
+// }
 function solution(absolutes, signs) {
-    let sum = 0;
-    absolutes.map((e,index) => {
-        if(signs[index]) sum += e;
-        else sum += -e;
-    })
-    return sum;
+    return absolutes.reduce((acc, value, index) => acc + (value*(signs[index]?1:-1)), 0)
 }
 console.log(solution([4,7,12], [true,false,true]))
