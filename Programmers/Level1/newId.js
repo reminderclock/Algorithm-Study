76e270290a4cbdb4db7a779a08355f3220b1c52a
reminderclock <reminderclock@naver.com>
2021-07-12 21:28:41 +0900

    chore: 로직작성

--- Programmers/Level1/newId.js
+++ Programmers/Level1/newId.js
@@ -1,13 +1,14 @@
 
+// 로직
 function solution(new_id) {
     const id = new_id
-        .toLowerCase()
-        .replace(/[^\w\d-_.]/g, '')
-        .replace(/\.{2,}/g, '.')
-        .replace(/^\.|\.$/g, '')
-        .padEnd(1, 'a')
-        .slice(0, 15)
-        .replace(/^\.|\.$/g, '')        
-    return id.padEnd(3, id[id.length-1])
+        .toLowerCase() // 소문자로 변환
+        .replace(/[^\w\d-_.]/g, '') // 문자, 숫자 ,- , _, .을 제외한것 "" 으로 대체
+        .replace(/\.{2,}/g, '.') // .이 최소 2이상이면 . 으로 대체
+        .replace(/^\.|\.$/g, '') //가장 앞이나 뒤에 .이 있으면 ''으로 대체
+        .padEnd(1, 'a') // 1 길이의 지정문자열 추가
+        .slice(0, 15) // 1부터 15 길이 까지 자름
+        .replace(/\.$/g, '')    // 마지막 . 있으면 "" 대체
+    return id.padEnd(3, id[id.length-1]) 
 }
 console.log(solution("...!@BaT#*..y.abcdefghijklm"));
