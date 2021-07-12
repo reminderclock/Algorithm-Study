2a0b93f8b2eff8fac0b56b11b458534945fd10a6
reminderclock <reminderclock@naver.com>
2021-07-12 21:20:15 +0900

    feat:[programmers] level1 신규아이디 추천

--- Programmers/Level1/newId.js
+++ Programmers/Level1/newId.js
@@ -1,31 +1,13 @@
-function solution(new_id) {
-    let answer = '';
-    let check_lower = /[a-z]/;
-    const incluArr = ['-', '_', '.'];
-    // for(let i=0; i<new_id.length; i++) {
-    //     if(new_id[i].toLowerCase() !== new_id[i]) {
-    //         answer += new_id[i].toLowerCase();
-    //     }
-    //     else if(incluArr.includes(new_id(i))) {
 
-    //     }
-    //     else answer += new_id[i];
-    // }
-    let arr = new_id.split('');
-    let a =  arr
-    .map(v => {
-        if(!isNaN(v)) return Number(v);
-        else return v.toLowerCase()
-    })
-    .filter(v => {
-        return !isNaN(v) || check_lower.test(v) || incluArr.includes(v)
-    })
-    // for(let i=0; i<a.length-1; i++) {
-    //     if()
-    // }
-    if(a[0] === undefined) a = ['a'];
-    if(a.length >= 16) a.splice(14)
-    return a;
+function solution(new_id) {
+    const id = new_id
+        .toLowerCase()
+        .replace(/[^\w\d-_.]/g, '')
+        .replace(/\.{2,}/g, '.')
+        .replace(/^\.|\.$/g, '')
+        .padEnd(1, 'a')
+        .slice(0, 15)
+        .replace(/^\.|\.$/g, '')        
+    return id.padEnd(3, id[id.length-1])
 }
-console.log(solution(".2..!@BaT#*..y.abcdefghijklm"))
-console.log(solution(""))
+console.log(solution("...!@BaT#*..y.abcdefghijklm"));
