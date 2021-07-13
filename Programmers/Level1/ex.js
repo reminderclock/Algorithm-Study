ce5097f27039150bac0bca05793251819f251ddf
reminderclock <reminderclock@naver.com>
2021-07-13 23:53:24 +0900

    chore: 주석

--- Programmers/Level1/ex.js
+++
@@ -1,4 +0,0 @@
-function solution(absolutes, signs) {
-    return absolutes.reduce((acc, value, index) => acc + (value*(signs[index]?1:-1)), 0)
-}
-console.log(solution([4,7,12], [true,false,true]))
