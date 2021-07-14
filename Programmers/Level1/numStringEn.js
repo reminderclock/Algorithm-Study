9241117978f8964e7b036976d0fe5bbcfee0cd54
reminderclock <reminderclock@naver.com>
2021-07-15 01:01:53 +0900

    faet: 숫자문자열과 영단어 result

--- Programmers/Level1/numStringEn.js
+++ Programmers/Level1/numStringEn.js
@@ -1,11 +1,10 @@
 function solution(s) {
 let str = s;
-let a = '';
 const alpa = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-for(let i=0; i<alpa.length; i++) {
-    a = str.replace(/[alpa[i]]/gi."")
+for(let i=0; i<alpa.length; i++) str = replaceALL(str,alpa[i],i)
+return parseInt(str);
 }
-return a;
-// console.log(str.split('4'))
+function replaceALL(str,a, b) {
+    return str.split(a).join(b)
 }
-console.log(solution("one4seveneight"))
+console.log(solution("one4oneseveneight"))
