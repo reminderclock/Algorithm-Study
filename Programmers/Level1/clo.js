bdc15dacf075543f3c45d8cd3213ce3bbaa29c1f
Luke <reminderclock@naver.com>
2021-09-05 20:00:49 +0900

    feat: 체육복1

--- Programmers/Level1/clo.js
+++ Programmers/Level1/clo.js
@@ -16,3 +16,25 @@ function solution(n, lost, reserve) {
 }
 solution(5, [2, 4], [1, 3, 5]);
 solution(5, [2, 4], [3]);
+
+function solution(n, lost, reserve) {
+  var answer = 0;
+  let lostArr = lost.filter((e) => !reserve.includes(e)).sort((a, b) => a - b);
+  let reserveArr = reserve
+    .filter((e) => !lost.includes(e))
+    .sort((a, b) => a - b);
+  let lostLength = lostArr.length;
+  let reserveLength = reserveArr.length;
+
+  for (const e of lostArr) {
+    if (lostLength === 0) break;
+    for (const data of reserveArr) {
+      if (Math.abs(e - data) === 1) {
+        lostLength--;
+        reserveArr = reserveArr.filter((el) => el !== data);
+        break;
+      }
+    }
+  }
+  return n - lostLength;
+}
