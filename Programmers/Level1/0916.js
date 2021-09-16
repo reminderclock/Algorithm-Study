3aafe069341661f07a4a355bd8981ec2cb38c9f4
Luke <reminderclock@naver.com>
2021-09-16 22:02:43 +0900

    add:소수 만들기2

--- Programmers/Level1/0916.js
+++ Programmers/Level1/0916.js
@@ -105,3 +105,20 @@ function solution(nums) {
   }
   return sum;
 }
+
+function isPrime(num) {
+  for (let i = 2, s = Math.sqrt(num); i <= s; i++)
+    if (num % i === 0) return false;
+  return num > 1;
+}
+function solution(nums) {
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        if (isPrime(nums[i] + nums[j] + nums[k])) sum++;
+      }
+    }
+  }
+  return sum;
+}
