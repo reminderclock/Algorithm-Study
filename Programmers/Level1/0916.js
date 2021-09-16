0f28c9b188c3d78d238d101156957e54718cd9e8
Luke <reminderclock@naver.com>
2021-09-16 21:59:03 +0900

    add: 핸드폰 번호가리기1

---
+++ Programmers/Level1/0916.js
@@ -0,0 +1,11 @@
+// 핸드폰 번호 가리기
+function solution(phone_number) {
+  return phone_number
+    .split("")
+    .map((e, index) => {
+      if (phone_number.length - index > 4) e = "*";
+      return e;
+    })
+    .join("");
+}
+solution("01033334444");
