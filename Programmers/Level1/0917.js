3d5c661ccd5778a9494f9c13aab9d594ccfcd821
Luke <reminderclock@naver.com>
2021-09-18 02:43:08 +0900

    feat: 4

--- Programmers/Level1/0917.js
+++ Programmers/Level1/0917.js
@@ -16,3 +16,45 @@ function solution(price, money, count) {
       .reduce((acc, curr) => acc + curr, 0) - money;
   return answer > 0 ? answer : 0;
 }
+
+//
+function solution(table, languages, preference) {
+  let max = 0;
+  return table
+    .map((e) => {
+      let [first, ...second] = e.split(" ");
+      e = [first, second];
+      return e.map((e2, index2) => {
+        let cnt = 1;
+        let array2 = [];
+        if (index2 > 0)
+          e2 = e2
+            .map((e, index3, array) => {
+              if (languages.indexOf(e) !== -1)
+                cnt = preference[languages.indexOf(e)];
+              return (array.length - index3) * cnt;
+            })
+            .filter((e4, index4, array) => languages.includes(e2[index4]))
+            .reduce((acc, curr) => acc + curr, 0);
+        return e2;
+      });
+    })
+    .map((e, index) => {
+      if (max < e[1]) max = e[1];
+      return e;
+    })
+    .filter((e, index) => max === e[1])
+    .map((e) => e[0])
+    .sort()[0];
+}
+solution(
+  [
+    "SI JAVA JAVASCRIPT SQL PYTHON C#",
+    "CONTENTS JAVASCRIPT JAVA PYTHON SQL C++",
+    "HARDWARE C C++ PYTHON JAVA JAVASCRIPT",
+    "PORTAL JAVA JAVASCRIPT PYTHON KOTLIN PHP",
+    "GAME C++ C# JAVASCRIPT C JAVA",
+  ],
+  ["PYTHON", "C++", "SQL"],
+  [7, 5, 5]
+);
